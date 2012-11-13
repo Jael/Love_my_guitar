6d5aa2e4ef837484f2d5e495326e13a72b499dd9
Jael <cutejael@gmail.com>
2012-11-13 23:46:13 +0800

    fix the bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :url, :tag, :type_id, :text
+  attr_accessible :title, :url, :tag, :type_id
   validates_presence_of :title
+  validates_presence_of :url 
   has_many :comments
   belongs_to :type
   belongs_to :user
   has_reputation :votes, source: :user, aggregated_by: :sum
   
-
 end
